7d75341e3f9099912ab85152ee634f908a24f0b8
Alexander Laudino <93599746+alexander-laudino@users.noreply.github.com>
2022-06-09 20:06:26 -0400

    Add keydown EventListeners to left and right pass divs

--- js/javascript.js
+++ js/javascript.js
@@ -3,3 +3,13 @@ const right = document.querySelector(".pass-right");
 
 let leftPass;
 let rightPass;
+
+left.addEventListener(
+  "keydown",
+  () => (leftPass = document.getElementById("pass").value)
+);
+
+right.addEventListener(
+  "keydown",
+  () => (rightPass = document.getElementById("pass").value)
+);
